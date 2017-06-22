a82ee71cf8f2384f025cdc53bc314ae49ea5f8f0
wtc-micky <2581635005@qq.com>
2017-06-23 01:12:11 +0800

    123
    321

---
+++ 3/src/sqldb/Db.java
@@ -0,0 +1,8 @@
+package sqldb;
+
+public class Db {
+    public String url = "jdbc:odbc:databaseName";
+    public String driver = "sun.jdbc.odbc.JdbcOdbcDriver";
+    public String user = "guest";
+    public String password = "guest"; 
+}
